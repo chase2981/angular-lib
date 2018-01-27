e40e0d2534de9bfd2cf4535d05a90a4993165206
Chase Gibbons <chasegibbons@Chase-Gibbons-MacBook-Pro.local>
2018-01-26 17:58:45 -0700

    cleaned up bash scripts a little bit

--- test.sh
+++ test.sh
@@ -1,22 +1,19 @@
-# Declaration(s)
+#!/bin/bash
+
+# declaration(s)
 GULP="node node_modules/.bin/gulp"
 KARMA="node node_modules/.bin/karma"
 NGC="node node_modules/.bin/ngc"
 TSC="node node_modules/.bin/tsc"
 
-# Clean up previous distributions
+# clean up previous distributions
 rm -rf ./dist ./build
 
-# Run gulp inline-templates
+# run gulp inline-templates
 $GULP inline-templates --gulpfile ./.gulpfile.js
 
-# Run Angular Compiler
+# run typescript compiler
 $TSC -p ./tsconfig-spec.json
-
-# rsync -a --exclude=*.ts src/ build/
-
-# Copy non-ts files from src to dist
-# rsync -a --exclude=*.ts src/app dist
 
 # start karma
 $KARMA start ./.karma.conf.js --single-run
